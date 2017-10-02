eac8ca661f0385277a7af18f59f3dbd95729c523
Tiago Lopes <tiagojavascript@gmail.com>
2017-10-02 02:15:39 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function theBeatlesPlay(musicians, instruments){
+  var beatlesPlayArray = [];
+  for (var i = 0; i < musicians.length; i++) {
+    beatlesPlayArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  };
+  return beatlesPlayArray;
+}
+
+function johnLennonFacts(array){
+  var johnLennonArray = [];
+  var i = 0;
+  while (i < array.length){
+    johnLennonArray.push(`${array[i]}!!!`)
+    i++
+  };
+  return johnLennonArray
+}
+
+function iLoveTheBeatles(n){
+  var loveTheBeatlesArray = [];
+  do{
+    loveTheBeatlesArray.push('I love the Beatles!')
+  } while(loveTheBeatlesArray.length <= n);
+    return loveTheBeatlesArray;
+}
